5ec7a422b49ae045e20c9be79bc2ef65860619df
Cosimo <cosi@graswald-schaetzle.com>
2026-03-19 15:54:31 +0100

    fix: jsonwebtoken 9.0.3 + node23 base image + esbuild externals (fixes 503 crash on startup)

--- build.js
+++ build.js
@@ -3,8 +3,16 @@ require('esbuild')
     entryPoints: ['server.js'],
     bundle: true,
     platform: 'node',
-    target: ['node23'],
+    target: ['node22'],
     outfile: 'dist/server.js',
-    external: ['swagger-ui-express', 'dotenv'],
+    external: [
+      'swagger-ui-express',
+      'dotenv',
+      '@google-cloud/storage',
+      '@google-cloud/batch',
+      'multer',
+      'jsonwebtoken',
+      'buffer-equal-constant-time',
+    ],
   })
   .catch(() => process.exit(1));
